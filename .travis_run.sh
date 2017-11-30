d1f79a25cf4c0e8161ea98669e7afca805022642
gkarvounis <george.c.karvounis@gmail.com>
2017-11-30 22:12:03 +0200

    Switch to cmake

--- .travis_run.sh
+++ .travis_run.sh
@@ -1,4 +1,4 @@
 cd .travis_docker
- docker build -t libmodbus/build:latest .
+docker build -t libmodbus/build:latest .
 docker run --rm -v $TRAVIS_BUILD_DIR:/root/libmodbusc++ libmodbus/build:latest
 
