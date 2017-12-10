31859e13539a0f7781b8c88d634ee343d5a84e95
gkarvounis <george.c.karvounis@gmail.com>
2017-12-10 02:34:09 +0200

    Add ability for different test server devices in order to support testing various scenarios

--- test/serverDevice/testServerDevice.cpp
+++ test/serverDevice/testServerDevice.cpp
@@ -8,32 +8,81 @@
 
 class TestServerDevice : public modbus::tcp::ServerDevice {
 public:
-                            TestServerDevice(const modbus::tcp::UnitId& id) : modbus::tcp::ServerDevice(id) {}
+                            TestServerDevice(const modbus::tcp::UnitId& id, std::function<bool(uint16_t address)> getCoilCb);
 protected:
-    virtual bool            getCoil(const modbus::tcp::Address& address) const override {
-        static const std::vector<bool> coils{1,0,1,0,0,0,0,0};
-        return coils.at(address.get()-0x1020);
-    }
+    virtual bool            getCoil(const modbus::tcp::Address& address) const override;
 
-    virtual bool            getDiscreteInput(const modbus::tcp::Address& address) const override {}
-    virtual uint16_t        getHoldingRegister(const modbus::tcp::Address& addr) const override {}
-    virtual uint16_t        getInputRegister(const modbus::tcp::Address& addr) const override {};
-
-    virtual void            setCoil(const modbus::tcp::Address& address, bool value) override {}
-    virtual void            setRegister(const modbus::tcp::Address& address, uint16_t value) override {}
-
-    virtual void            setCoils(const modbus::tcp::Address& startAddress, const std::vector<bool>& coils) override {}
-    virtual void            setRegisters(const modbus::tcp::Address& startAddress, const std::vector<uint16_t>& regs) override {}
+    std::function<bool(uint16_t address)> m_getCoilCb;
 };
+
+
+TestServerDevice::TestServerDevice(const modbus::tcp::UnitId& id, std::function<bool(uint16_t address)> getCoilCb) :
+    modbus::tcp::ServerDevice(id),
+    m_getCoilCb(getCoilCb)
+{
+}
+
+
+bool TestServerDevice::getCoil(const modbus::tcp::Address& address) const {
+    return m_getCoilCb(address.get());
+}
 
 
 TEST_CASE("handle read coils", "[server device]") {
     namespace mt = modbus::tcp;
 
-    std::vector<uint8_t> rsp;
-    TestServerDevice dev(mt::UnitId(0xab));
+    TestServerDevice dev(mt::UnitId(0xab), [](uint16_t address)->bool {
+        static const std::vector<bool> coils{1,0,1,0,0,0,0,0};
+        return coils.at(address-0x1020);
+    });
 
-    dev.handleMessage({0x00, 0x02, 0x00, 0x00, 0x00, 0x06, 0xab, 0x01, 0x10, 0x20, 0x00, 0x04}, rsp);
+    std::vector<uint8_t> req;
+    std::vector<uint8_t> rsp;
+
+    mt::Encoder encoder(mt::UnitId(0xab), mt::TransactionId(0x02));
+    encoder.encodeReadCoilsReq(mt::Address(0x1020), mt::NumBits(4), req);
+    dev.handleMessage(req, rsp);
 
     REQUIRE(rsp == (std::vector<uint8_t>{0x00, 0x02, 0x00, 0x00, 0x00, 0x04, 0xab, 0x01, 0x01, 0b00000101}));
+}
+
+
+TEST_CASE("handle read coils - bad unit id", "[server device]") {
+    namespace mt = modbus::tcp;
+
+    TestServerDevice dev(mt::UnitId(0xab), [](uint16_t address)->bool {
+        static const std::vector<bool> coils{1,0,1,0,0,0,0,0};
+        return coils.at(address-0x1020);
+    });
+
+    std::vector<uint8_t> req;
+    std::vector<uint8_t> rsp;
+
+    mt::Encoder encoder(mt::UnitId(0xac), mt::TransactionId(0x02));
+    encoder.encodeReadCoilsReq(mt::Address(0x1020), mt::NumBits(4), req);
+    REQUIRE_THROWS_AS(dev.handleMessage(req, rsp), mt::UnitIdMismatch);
+}
+
+
+TEST_CASE("handle read coils - operation not supported") {
+    namespace mt = modbus::tcp;
+
+    TestServerDevice dev(mt::UnitId(0xab), [](uint16_t address)->bool {
+        throw mt::FunctionCodeNotSupported(mt::FunctionCode::READ_COILS);
+    });
+
+    std::vector<uint8_t> req;
+    std::vector<uint8_t> rsp;
+
+    mt::Encoder encoder(mt::UnitId(0xab), mt::TransactionId(0x02));
+    encoder.encodeReadCoilsReq(mt::Address(0x1020), mt::NumBits(4), req);
+    dev.handleMessage(req, rsp);
+
+    mt::decoder_views::Header rsp_header_view(rsp);
+    REQUIRE(rsp_header_view.isError() == true);
+    REQUIRE(rsp_header_view.getTransactionId() == mt::TransactionId(0x02));
+    REQUIRE(rsp_header_view.getUnitId() == mt::UnitId(0xab));
+
+    mt::decoder_views::ErrorResponse rsp_payload_view(rsp);
+    REQUIRE(rsp_payload_view.getCode() == mt::ExceptionCode::ILLEGAL_FUNCTION);
 }
