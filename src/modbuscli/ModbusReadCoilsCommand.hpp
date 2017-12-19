ed82c9fbf2212e3dc27afe4ba847791feeee786e
gkarvounis <george.c.karvounis@gmail.com>
2017-12-19 23:58:10 +0200

    Add positional args for readcoils command

--- src/modbuscli/ModbusReadCoilsCommand.hpp
+++ src/modbuscli/ModbusReadCoilsCommand.hpp
@@ -7,16 +7,20 @@
 
 class ReadCoilsCommand : public ModbusCommand {
 public:
-    inline                      ReadCoilsCommand();
-    void                        exec(ModbusClient& client, const std::vector<std::string>& args) override;
-    std::string                 getShortHelpText() const override;
-    std::string                 getHelpText() const override;
+    inline                          ReadCoilsCommand();
+    void                            exec(ModbusClient& client, const std::vector<std::string>& args) override;
+    std::string                     getShortHelpText() const override;
+    std::string                     getHelpText() const override;
 
 private:
-    uint16_t                    m_startAddress;
-    uint16_t                    m_numCoils;
+    using OptionsDescription = boost::program_options::options_description;
+    using PositionalOptionsDescription = boost::program_options::positional_options_description;
 
-    boost::program_options::options_description m_options;
+    uint16_t                        m_startAddress;
+    uint16_t                        m_numCoils;
+
+    OptionsDescription              m_options;
+    PositionalOptionsDescription    m_positional_options;
 };
 
 
@@ -29,6 +33,9 @@ ReadCoilsCommand::ReadCoilsCommand() :
     m_options.add_options()
         ("startAddress,s", po::value<uint16_t>(&m_startAddress)->required(), "address of first coil to retrieve")
         ("numCoils,n", po::value<uint16_t>(&m_numCoils)->required(), "number of coils to retrieve (up to 2000 coils)");
+
+    m_positional_options.add("startAddress", 1);
+    m_positional_options.add("numCoils", 1);
 }
 
 
@@ -36,8 +43,7 @@ void ReadCoilsCommand::exec(ModbusClient& client, const std::vector<std::string>
     namespace po = boost::program_options;
 
     po::variables_map vm;
-    //po::store(po::command_line_parser(args).options(m_options).positional(m_positional_options).run(), vm);
-    po::store(po::command_line_parser(args).options(m_options).run(), vm);
+    po::store(po::command_line_parser(args).options(m_options).positional(m_positional_options).run(), vm);
     po::notify(vm);
 
     client.readCoils(modbus::tcp::Address(m_startAddress), modbus::tcp::NumBits(m_numCoils));
@@ -50,7 +56,11 @@ std::string ReadCoilsCommand::getShortHelpText() const {
 
 
 std::string ReadCoilsCommand::getHelpText() const {
-    return "getcoils [options]";
+    std::stringstream ss;
+    ss << getShortHelpText() << std::endl
+       << "Usage:" << std::endl
+       << "    readcoils <start_address> <num_coils>" << std::endl;
+    return ss.str();
 }
 
 #endif
