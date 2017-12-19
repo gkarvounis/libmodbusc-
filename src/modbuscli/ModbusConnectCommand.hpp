bc9e32924f48fbfb741582f958de18f8d7a327f2
gkarvounis <george.c.karvounis@gmail.com>
2017-12-19 23:56:54 +0200

    Add positional options for connect

--- src/modbuscli/ModbusConnectCommand.hpp
+++ src/modbuscli/ModbusConnectCommand.hpp
@@ -3,15 +3,19 @@
 
 class ModbusConnectCommand : public ModbusCommand {
 public:
-                                ModbusConnectCommand();
+                                    ModbusConnectCommand();
 
-    void                        exec(ModbusClient& client, const std::vector<std::string>& args) override;
-    std::string                 getShortHelpText() const override;
-    std::string                 getHelpText() const override;
+    void                            exec(ModbusClient& client, const std::vector<std::string>& args) override;
+    std::string                     getShortHelpText() const override;
+    std::string                     getHelpText() const override;
 private:
-    std::string                 m_address;
-    uint16_t                    m_port;
-    boost::program_options::options_description m_options;
+    using OptionsDescription = boost::program_options::options_description;
+    using PositionalOptionsDescription = boost::program_options::positional_options_description;
+
+    std::string                     m_address;
+    uint16_t                        m_port;
+    OptionsDescription              m_options;
+    PositionalOptionsDescription    m_positional_options;
 };
 
 
@@ -23,14 +27,17 @@ ModbusConnectCommand::ModbusConnectCommand() :
     m_options.add_options()
         ("address,a", po::value<std::string>(&m_address)->default_value("127.0.0.1"), "ip address of target device")
         ("port,p", po::value<uint16_t>(&m_port)->default_value(502), "tcp connection port");
+
+    m_positional_options.add("address", 1);
+    m_positional_options.add("port", 1);
 }
+
 
 void ModbusConnectCommand::exec(ModbusClient& client, const std::vector<std::string>& args) {
     namespace po = boost::program_options;
 
     po::variables_map vm;
-    //po::store(po::command_line_parser(args).options(m_options).positional(m_positional_options).run(), vm);
-    po::store(po::command_line_parser(args).options(m_options).run(), vm);
+    po::store(po::command_line_parser(args).options(m_options).positional(m_positional_options).run(), vm);
     po::notify(vm);
 
     client.connect(boost::asio::ip::tcp::endpoint(boost::asio::ip::address::from_string(m_address), m_port));
@@ -44,10 +51,12 @@ std::string ModbusConnectCommand::getShortHelpText() const {
 
 std::string ModbusConnectCommand::getHelpText() const {
     std::stringstream ss;
+
     ss << "Open connection to a modbus device." << std::endl
-    << "Usage:" << std::endl
-    << "    connect -a <ip address> -p <port>" << std::endl
-    << m_options;
+       << "Usage:" << std::endl
+       << "    connect [<ip address>] [<port>]" << std::endl
+       << "Default values are 127.0.0.1 and port 502" << std::endl;
+
     return ss.str();
 }
 
