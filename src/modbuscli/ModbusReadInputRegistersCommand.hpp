cb84c7bff67e9df851f5280dbb2222e211496086
gkarvounis <george.c.karvounis@gmail.com>
2017-12-23 22:22:12 +0000

    Add polling support to readinputregs command

--- src/modbuscli/ModbusReadInputRegistersCommand.hpp
+++ src/modbuscli/ModbusReadInputRegistersCommand.hpp
@@ -14,6 +14,7 @@ private:
 
     uint16_t                        m_startAddress;
     uint16_t                        m_numRegs;
+    std::size_t                     m_interval;
 
     OptionsDescription              m_options;
     PositionalOptionsDescription    m_positional_options;
@@ -26,10 +27,12 @@ ModbusReadInputRegistersCommand::ModbusReadInputRegistersCommand() {
 
     m_options.add_options()
         ("startAddress,s", po::value<uint16_t>(&m_startAddress)->required(), "address of first coil to retrieve")
-        ("numRegs,n", po::value<uint16_t>(&m_numRegs)->required(), "number of coils to retrieve (up to 2000 coils)");
+        ("numRegs,n", po::value<uint16_t>(&m_numRegs)->required(), "number of coils to retrieve (up to 2000 coils)")
+        ("interval,i", po::value<std::size_t>(&m_interval)->default_value(0), "polling interval in msecs");
 
     m_positional_options.add("startAddress", 1);
     m_positional_options.add("numRegs", 1);
+    m_positional_options.add("interval", 1);
 }
 
 
@@ -40,7 +43,15 @@ void ModbusReadInputRegistersCommand::exec(ModbusClient& client, const std::vect
     po::store(po::command_line_parser(args).options(m_options).positional(m_positional_options).run(), vm);
     po::notify(vm);
 
-    client.readInputRegisters(modbus::tcp::Address(m_startAddress), modbus::tcp::NumRegs(m_numRegs));
+    if (m_interval == 0)
+        client.readInputRegisters(modbus::tcp::Address(m_startAddress), modbus::tcp::NumRegs(m_numRegs));
+    else {
+        while (true) {
+            client.readInputRegisters(modbus::tcp::Address(m_startAddress), modbus::tcp::NumRegs(m_numRegs));
+            std::cout << std::endl;
+            usleep(m_interval * 1000);
+        }
+    }
 }
 
 
@@ -54,7 +65,8 @@ std::string ModbusReadInputRegistersCommand::getHelpText() const {
 
     ss << getShortHelpText() << std::endl
        << "Usage:" << std::endl
-       << "    readinputregs <start address> <number of registers>" << std::endl;
+       << "    readinputregs <start address> <number of registers> [<interval>]" << std::endl
+       << "Last parameter, if present and non-zero, is interpreted as polling interval in msecs" << std::endl;
 
     return ss.str();
 }
