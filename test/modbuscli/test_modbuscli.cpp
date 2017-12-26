08ea1b0945205ef5724ce45c09ab72abe4dc0457
gkarvounis <george.c.karvounis@gmail.com>
2017-12-26 23:54:12 +0000

    Add dummy test for modbuscli

---
+++ test/modbuscli/test_modbuscli.cpp
@@ -0,0 +1,15 @@
+#define CATCH_CONFIG_MAIN
+
+#include "Catch.hpp"
+#include "ModbusConsts.hpp"
+#include "ModbusTypes.hpp"
+#include "ModbusEncoder.hpp"
+#include "ModbusDecoder.hpp"
+#include "OutputFormatter.hpp"
+#include "VerboseStandardOutputFormatter.hpp"
+#include "ModbusCommand.hpp"
+#include "ModbusReadCoilsCommand.hpp"
+
+TEST_CASE("cli", "[cli]") {
+}
+
